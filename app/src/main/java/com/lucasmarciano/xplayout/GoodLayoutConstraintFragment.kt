8b2b66f8d96fcfc2df074221b5327f406d79fa4f
SERVIDORES\lucasmarciano <lucasmarciano@inatel.br>
2018-07-25 17:52:06 -0300

    Estrutura dos fragments

---
+++ app/src/main/java/com/lucasmarciano/xplayout/GoodLayoutConstraintFragment.kt
@@ -0,0 +1,22 @@
+package com.lucasmarciano.xplayout
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class GoodLayoutConstraintFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_good_layout_constraint, container, false)
+    }
+}
+
+
